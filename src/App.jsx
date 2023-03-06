fb503d293fa622cf681f39fa31e67dc3e60fd0c7
James McGrory <james.mcgrory135@gmail.com>
2023-03-06 20:18:43 +1100

    scoring system functioning

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,32 @@ function App() {
   const API = "https://opentdb.com/api.php?amount=5&category=9&type=multiple"
   
   const [questionData, setQuestionData] = useState([])
+  const [questionNum, setQuestionNum] = useState(0)
+  const [playerSelected, setPlayerSelected] = useState("")
+  const [score, setScore] = useState(0)
+
+  function nextQuestion() {
+    {/* Check if players choice is equal to the correct answer, increment score if true */}
+    if(playerSelected == questionData[questionNum].correctAnswer) {
+      setScore(prevScore => prevScore + 1)
+    }
+    
+    {/* Increment through the questionData array when button pressed*/}
+    if(questionNum < questionData.length - 1) {
+      setQuestionNum(prevNum => prevNum + 1)
+    }
+    else {
+      alert("End of Quiz")
+    }
+  }
 
   useEffect(() => {
     fetch(API)
       .then(res => res.json())
       .then(data => setQuestionData(data.results.map(question => {
         return {
-          question: question.question,
-          correctAnswer: question.correct_answer,
+          question: decode(question.question),
+          correctAnswer: decode(question.correct_answer),
           incorrectAnswers: question.incorrect_answers,
           //sorting the array into a random order
           answerArray: [...question.incorrect_answers, question.correct_answer].sort((a,b) => 0.5 - Math.random())
@@ -22,20 +40,32 @@ function App() {
       })))
   },[])
 
-  console.log(questionData[0])
+  console.log(playerSelected)
+
   return (
     <div>
-      <h1 className='text-red-100'>Test</h1>
       {questionData[0] != null && 
         <div>
           <h2>
-            {decode(questionData[0].question)}
+            {questionData[questionNum].question}
           </h2>
           <div className='flex flex-col'>
-            <button>{questionData[0].answerArray[0]}</button>
-            <button>{questionData[0].answerArray[1]}</button>
-            <button>{questionData[0].answerArray[2]}</button>
-            <button>{questionData[0].answerArray[3]}</button>
+            {/* Render a button for each answer in the answersArray */}
+            {questionData[questionNum].answerArray.map(answer => {
+              return <button
+                        key={answer} 
+                        onClick={(e) => setPlayerSelected(e.target.textContent)} 
+                        className='bg-violet-200 w-1/2 mt-2 mx-auto hover:bg-violet-400 focus:bg-violet-500'>
+                          {decode(answer)}
+                      </button>
+            })}
+          </div>
+          <div>
+            <button
+              onClick={nextQuestion} 
+              className='bg-teal-300 hover:bg-teal-400'>
+              Next Question
+            </button>
           </div>
         </div>
       }
